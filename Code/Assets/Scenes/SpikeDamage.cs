0bc49b4607b44e9707a005da75f895df9e415b77
Fady Nabil <35051247+fadynabil10@users.noreply.github.com>
2019-09-20 23:47:42 +0200

    Add files via upload

---
+++ Code/Assets/Scenes/SpikeDamage.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.GetComponent<Charachter>().Respawn();
+		}
+	}
+}
