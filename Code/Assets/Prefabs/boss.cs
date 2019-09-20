7eef7dcc1b70d91d8af0cb20efb7010ba61119d9
Fady Nabil <35051247+fadynabil10@users.noreply.github.com>
2019-09-20 23:45:20 +0200

    Add files via upload

---
+++ Code/Assets/Prefabs/boss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss : MonoBehaviour
+{
+	public bool move = false;
+	public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (move)
+		{
+			transform.Translate(Vector2.left * .07f);
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			collision.GetComponent<Charachter>().Respawn();
+		}
+	}
+}
